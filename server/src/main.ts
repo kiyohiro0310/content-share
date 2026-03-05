a6afafbd3f81f0cfe4209c813cdd40d241183cb5
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2026-03-06 03:02:26 +1100

    Modify main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const ENV = process.env.NODE_ENV;
+  console.log(ENV);
+
+  if (ENV !== 'development') {
+    app.setGlobalPrefix('api');
+  }
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
