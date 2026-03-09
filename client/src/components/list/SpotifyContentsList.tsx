c6c88cb8e0b7470e041ed555014b7555385c2a17
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2026-03-10 00:28:06 +1100

    Add artist name

--- client/src/components/list/SpotifyContentsList.tsx
+++ client/src/components/list/SpotifyContentsList.tsx
@@ -7,32 +7,32 @@ const SpotifyContentsList = ({ contents, onClick }: { contents: SpotifyContentTy
   const { addedContents, setAddedContents } = useAddedContent();
 
   const handleClick = async (name: string, img: string, url: string) => {
-      const today = new Date().toLocaleDateString("en-AU");
-      const dataExists = addedContents.some((content) => content.date === today);
-      const newContent = {
-        date: today,
-        items: [{ name, image: img, url, type: "spotify" }],
-      };
-      if (!dataExists) setAddedContents([...addedContents, newContent]);
-      else {
-        const updatedContents = [
-          ...addedContents.map((content) => {
-            if (content.date === today) {
-              return {
-                ...content,
-                items: [...content.items, { name, image: img, url, type: "spotify" }],
-              };
-            } else {
-              return content;
-            }
-          }),
-        ];
-        setAddedContents(updatedContents);
-      }
+    const today = new Date().toLocaleDateString("en-AU");
+    const dataExists = addedContents.some((content) => content.date === today);
+    const newContent = {
+      date: today,
+      items: [{ name, image: img, url, type: "spotify" }],
+    };
+    if (!dataExists) setAddedContents([...addedContents, newContent]);
+    else {
+      const updatedContents = [
+        ...addedContents.map((content) => {
+          if (content.date === today) {
+            return {
+              ...content,
+              items: [...content.items, { name, image: img, url, type: "spotify" }],
+            };
+          } else {
+            return content;
+          }
+        }),
+      ];
+      setAddedContents(updatedContents);
+    }
 
-      await storeContent({ name, image: img, url, type: "spotify", comments: []});
-      toast.success(`Song: ${name} has been added into playlist.`);
-      onClick();
+    await storeContent({ name, image: img, url, type: "spotify", comments: [] });
+    toast.success(`Song: ${name} has been added into playlist.`);
+    onClick();
   };
 
   return (
@@ -42,8 +42,8 @@ const SpotifyContentsList = ({ contents, onClick }: { contents: SpotifyContentTy
           {item ? (
             <div className="">
               <div className="cursor-pointer border-b-[0.5px] border-b-gray-400 dark:border-b-white">
-                <div className="flex flex-col md:flex-row md:items-center justify-between p-2">
-                  <div className="flex items-center space-x-4">
+                <div className="flex flex-col md:flex-row md:items-center justify-between p-2 pt-4">
+                  <div className="flex items-start space-x-4">
                     <img
                       className="w-16"
                       src={item.album.images[0].url}
@@ -51,7 +51,14 @@ const SpotifyContentsList = ({ contents, onClick }: { contents: SpotifyContentTy
                       width={item.album.images[0].width}
                       height={item.album.images[0].height}
                     />
-                    <h1>{item.name}</h1>
+                    <div className="flex flex-col justify-start items-start">
+                      <h1 className='text-xl'>{item.name}</h1>
+                      <div className='flex text-sm'>
+                        {item.artists.map((artist: { name: string }, index: number) => (
+                          <div key={index}>{artist.name}</div>
+                        ))}
+                      </div>
+                    </div>
                   </div>
 
                   <div className="flex items-center justify-center space-x-8">
